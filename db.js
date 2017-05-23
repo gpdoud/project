867c4f7cc1a892a8d16eb41bd3d0e1cfd3ac29ad
Greg <gpdoud@gmail.com>
2017-05-23 11:40:20 -0400

    added index.html and updated db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 // Database
 var db = {
-	Product: [
+	Products: [
 		{ Id: 1, Code: 'java', Description: 'Java Programming', ListPrice: '57.50'},
 		{ Id: 2, Code: 'jsp', Description: 'Java Servlets and JSP', ListPrice: '57.50'},
 		{ Id: 3, Code: 'mysql', Description: 'MySQL', ListPrice: '54.50'},
@@ -9,11 +9,18 @@ var db = {
 		{ Id: 6, Code: 'oracle', Description: 'Oracle and PL/SQL', ListPrice: '54.50'},
 		{ Id: 7, Code: 'javascript', Description: 'JavaScript and jQuery', ListPrice: '54.50'}	
 	],
-	Vendor: [
+	Vendors: [
 		{ Id: 1, Name: 'ABC Company' },
 		{ Id: 2, Name: 'DEF Company' },
 		{ Id: 3, Name: 'GHI Company' },
 		{ Id: 4, Name: 'JKL Company' },
 		{ Id: 5, Name: 'MNO Company' }
+	],
+	Requests: [
+		{ Id: 1 },
+		{ Id: 2 },
+		{ Id: 3 },
+		{ Id: 4 },
+		{ Id: 5 }
 	]
 }
