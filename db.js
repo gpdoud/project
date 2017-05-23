e49c3433ccf02f59d7d6a577e1bab383ddf94109
Greg <gpdoud@gmail.com>
2017-05-23 11:07:53 -0400

    Initial commit - db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+// Database
+var db = {
+	Product: [
+		{ Id: 1, Code: 'java', Description: 'Java Programming', ListPrice: '57.50'},
+		{ Id: 2, Code: 'jsp', Description: 'Java Servlets and JSP', ListPrice: '57.50'},
+		{ Id: 3, Code: 'mysql', Description: 'MySQL', ListPrice: '54.50'},
+		{ Id: 4, Code: 'android', Description: 'Android Programming', ListPrice: '57.50'},
+		{ Id: 5, Code: 'html5', Description: 'HTML5 and CSS3', ListPrice: '54.50'},
+		{ Id: 6, Code: 'oracle', Description: 'Oracle and PL/SQL', ListPrice: '54.50'},
+		{ Id: 7, Code: 'javascript', Description: 'JavaScript and jQuery', ListPrice: '54.50'}	
+	],
+	Vendor: [
+		{ Id: 1, Name: 'ABC Company' },
+		{ Id: 2, Name: 'DEF Company' },
+		{ Id: 3, Name: 'GHI Company' },
+		{ Id: 4, Name: 'JKL Company' },
+		{ Id: 5, Name: 'MNO Company' }
+	]
+}
