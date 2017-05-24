bf94e959db3642bab7ec0a0536c5b842d2f2ecbf
Greg <gpdoud@gmail.com>
2017-05-24 16:28:34 -0400

    db.js includes all the entities, but no data

--- db.js
+++ db.js
@@ -1,26 +1,23 @@
 // Database
 var db = {
-	Products: [
-		{ Id: 1, Code: 'java', Description: 'Java Programming', ListPrice: '57.50'},
-		{ Id: 2, Code: 'jsp', Description: 'Java Servlets and JSP', ListPrice: '57.50'},
-		{ Id: 3, Code: 'mysql', Description: 'MySQL', ListPrice: '54.50'},
-		{ Id: 4, Code: 'android', Description: 'Android Programming', ListPrice: '57.50'},
-		{ Id: 5, Code: 'html5', Description: 'HTML5 and CSS3', ListPrice: '54.50'},
-		{ Id: 6, Code: 'oracle', Description: 'Oracle and PL/SQL', ListPrice: '54.50'},
-		{ Id: 7, Code: 'javascript', Description: 'JavaScript and jQuery', ListPrice: '54.50'}	
+	Product: [
+		{ Id: 1, VendorId: 1, Name: '', PartNumber: '', Price: 1.00
+			, Unit: '', PhotoPath: '' },
 	],
-	Vendors: [
-		{ Id: 1, Name: 'ABC Company' },
-		{ Id: 2, Name: 'DEF Company' },
-		{ Id: 3, Name: 'GHI Company' },
-		{ Id: 4, Name: 'JKL Company' },
-		{ Id: 5, Name: 'MNO Company' }
+	Vendor: [
+		{ Id: 1, Code: '', Name: '', Address: '', City: '', State: ''
+			, Zip: '', Phone: '', Email: '', IsPreApproved: 0 },
 	],
-	Requests: [
-		{ Id: 1 },
-		{ Id: 2 },
-		{ Id: 3 },
-		{ Id: 4 },
-		{ Id: 5 }
+	User: [
+		{ Id: 1, UserName: '', Password: '', FirstName: '', LastName: ''
+			, Phone: '', Email: '', IsManager: 0 },
+	],
+	Request: [
+		{ Id: 1, UserId: '', Description: '', Justification: ''
+			, DateNeeded: '', DeliveryMode: '', DocsAttached: 0
+			, Status: '', Total: 1.00, SubmittedDate: '' }, 
+	]
+	LineItem: [
+		{ Id: 1, RequestId: 1, ProductId: 1, Quantity: 0 },
 	]
 }
